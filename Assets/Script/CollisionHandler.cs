d408cc1c16f534527dc6198d61ae1e244b8ab4ba
Szymon Kilian <szymonkilian98@o2.pl>
2023-05-23 17:45:59 +0200

    Added collision handler

--- Assets/Script/CollisionHandler.cs
+++ Assets/Script/CollisionHandler.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    
+    void OnCollisionEnter(Collision other) {
         
+        switch(other.gameObject.tag){
+            case "Friendly":
+                Debug.Log("Collision with friendly target");
+                break;
+            case "Finish":
+                Debug.Log("Perfect Landing!");
+                break;
+            case "Start":
+                Debug.Log("On launching pad");
+                break;
+            default:
+                Debug.Log("BOOM");
+                ReloadLevel();
+                break;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void ReloadLevel()
     {
-        
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentLevel);
     }
 }
